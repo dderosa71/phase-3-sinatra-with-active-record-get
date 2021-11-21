c913505fdad02e91de739784beca55c34af7ea8e
dominicderosa <dderosa71@gmail.com>
2021-11-20 19:06:16 -0800

    commit tree 1a2952807c3cdf9bd5b28e3cc3f43633eda07e36

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,21 @@
 class ApplicationController < Sinatra::Base
 
-  get '/' do
-    { message: "Hello world" }.to_json
+  get '/games/:id' do
+    game = Game.find(params[:id])
+
+    # include associated reviews in the JSON response
+    game.to_json(only: [:id, :title, :genre, :price], include: {
+      reviews: { only: [:comment, :score], include: {
+        user: { only: [:name] }
+      } }
+    })
+  end
+
+  # use the :id syntax to create a dynamic route
+  get '/games/:id' do
+    game = Game.find(params[:id])
+    # send a JSON-formatted response of the game data
+    game.to_json
   end
 
 end
